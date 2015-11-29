d6895c8cd3bde04c78bfa0684984b6d18964183c
Oliver Brooks <oli@pearlshare.com>
2015-11-29 14:36:33 +0000

    handle giving Points constructor an empty array

--- test/points.js
+++ test/points.js
@@ -9,6 +9,11 @@ describe("Points", function () {
       var points = new Points(["51,21", "45,15"]);
       expect(points).to.be.a(Points);
     });
+
+    it("should construct a Points object from an empty array", function () {
+      var points = new Points([]);
+      expect(points).to.be.a(Points);
+    });
   });
 
   describe("getRectangle", function () {
@@ -20,6 +25,11 @@ describe("Points", function () {
     it("should calculate a rectangle", function () {
       var points = new Points(["40,20", "50,10"]);
       expect(points.getRectangle().eql(50, 20, 40, 10)).to.be(true);
+    });
+
+    it("should handle an empty array input", function () {
+      var points = new Points([]);
+      expect(points.getRectangle().eql(0, 0, 0, 0)).to.be(true);
     });
   });
 
